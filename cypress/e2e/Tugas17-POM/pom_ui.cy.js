8623afb4a7714fdf4879fd1e36f523ea5679f0c7
dewiokta <dewiokta48@gmail.com>
2026-07-16 00:33:53 +0700

    add POM

---
+++ cypress/e2e/Tugas17-POM/pom_ui.cy.js
@@ -0,0 +1,91 @@
+import loginPage from '../../support/Tugas17-POM/loginPage'
+import loginData from '../../fixtures/Tugas17-POM/loginData'
+
+describe('Test Scenario Login OrangeHRM', () => {
+
+    it('TC01-Login valid account', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.validUser.username)
+        loginPage.inputPassword(loginData.validUser.password)
+        loginPage.clickLoginButton()
+        loginPage.verifyLoginSuccess()
+    })
+
+    it('TC02-Login valid account, password is masked', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.validUser.username)
+        loginPage.inputPassword(loginData.validUser.password)
+        loginPage.passwordShouldBeMasked()
+        loginPage.clickLoginButton()
+        loginPage.verifyLoginSuccess() 
+    })
+
+    it('TC03-Login invalid account, wrong username', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.invalidUser.username)
+        loginPage.inputPassword(loginData.validUser.password)
+        loginPage.clickLoginButton()
+        loginPage.invalidLoginShouldShowError()
+        loginPage.loginFailed()
+    })
+
+    it('TC04-Login invalid account, wrong password', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.validUser.username)
+        loginPage.inputPassword(loginData.invalidUser.password)
+        loginPage.clickLoginButton()
+        loginPage.invalidLoginShouldShowError()
+        loginPage.loginFailed()
+    })
+
+
+    it('TC05-Login invalid account, username is empty', () => {
+        loginPage.visitPage()
+        loginPage.usernameClear
+        loginPage.inputPassword(loginData.validUser.password)
+        loginPage.clickLoginButton()    
+        loginPage.requiredField()
+        loginPage.notIncludeDashboardUrl()
+    })
+
+    it('TC06-Login invalid account, password is empty', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.validUser.username)
+        loginPage.passwordClear()
+        loginPage.clickLoginButton()
+        loginPage.requiredField()
+        loginPage.notIncludeDashboardUrl()
+    })
+
+    it('TC07-Login invalid account, username and password is empty', () => {
+        loginPage.visitPage()
+        loginPage.usernameClear()
+        loginPage.passwordClear()
+        loginPage.clickLoginButton()
+        loginPage.requiredField()
+        loginPage.notIncludeDashboardUrl()
+    })
+
+    it('TC08-Access dashboard without login', () => {
+        loginPage.visitDashboard()
+        loginPage.bypassNotAllowed()
+    })
+
+    it('TC09-Login invalid account, wrong password case-sensitivity', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.validUser.username)
+        loginPage.inputPassword(loginData.CaseSensitiveUser.password)
+        loginPage.clickLoginButton()
+        loginPage.invalidLoginShouldShowError()
+        loginPage.loginFailed()
+    })
+
+    it('TC10-Login invalid account, wrong username and password', () => {
+        loginPage.visitPage()
+        loginPage.inputUsername(loginData.invalidUser.username)
+        loginPage.inputPassword(loginData.invalidUser.password)
+        loginPage.clickLoginButton()
+        loginPage.invalidLoginShouldShowError()
+        loginPage.loginFailed()
+    })
+})
